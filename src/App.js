aac0b4687d035c1074c8d997fea4221205fed89c
Shubham Singh <Shubham16cse06@gmail.com>
2021-03-25 06:37:14 +0530

    header one with background image which have transparent Navbar

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header_1 from "./components/Header_1/Header_1";
+
+function App() {
+  return (
+    <div className="app">
+      <Header_1 />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header_1/Header_1.js
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Header_1.css";
+
+function Header_1() {
+  return (
+    <header id="home">
+      <nav id="nav-wrap">
+        <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
+          Show navigation
+        </a>
+        <a className="mobile-btn" href="#home" title="Hide navigation">
+          Hide navigation
+        </a>
+
+        <ul id="nav" className="nav">
+          <li className="current">
+            <a className="smoothscroll" href="#home">
+              Home
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#about">
+              About
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#resume">
+              Resume
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#portfolio">
+              Works
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#testimonials">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a className="smoothscroll" href="#contact">
+              Contact
+            </a>
+          </li>
+        </ul>
+      </nav>
+
+      <div className="banner">
+        <div className="banner-text">
+          <h1>This is Shubham Singh</h1>
+          <h3>I am here to work</h3>
+          <hr />
+        </div>
+      </div>
+
+      <p className="scrolldown">
+        <a className="smoothscroll" href="#about">
+          <i className="icon-down-circle"></i>
+        </a>
+      </p>
+    </header>
+  );
+}
+
+export default Header_1;
